77363aaf0aa42c20da0ba33e3973cd915135211a
karenpiscitello <karen.piscitello23@gmail.com>
2022-01-04 23:29:54 -0300

    modificacion

--- src/components/helpers/getFetch.jsx
+++ src/components/helpers/getFetch.jsx
@@ -1,7 +1,7 @@
 const productos = [ 
     {id: 1, imagen: '.images/REME1.PNG', nombre: 'remera', descripcion: 'Advengers', stock: 10},
-    {id: 1, imagen: '.images/reme2.PNG', nombre: 'remera', descripcion: 'Simpson', stock: 5},
-    {id: 1, imagen: '.images/reme3.PNG', nombre: 'remera', descripcion: 'Messi', stock: 10}
+    {id: 2, imagen: '.images/reme2.PNG', nombre: 'remera', descripcion: 'Simpson', stock: 5},
+    {id: 3, imagen: '.images/reme3.PNG', nombre: 'remera', descripcion: 'Messi', stock: 10}
 ];
 
 
