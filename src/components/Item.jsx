9309852b8ac3fe691d46d671763443de9d17c1f0
karenpiscitello <karen.piscitello23@gmail.com>
2022-01-13 22:01:01 -0300

    cambios imagenes

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,12 @@
 import React from "react"
-const Item = ({description, price, stock, title}) => {
+const Item = ({description, price, stock, title, image}) => {
     return (
         <div className="flex bg-info">
         <h3>{title}</h3>
         <h2>{price}</h2>
         <p>{description}</p>
         <h3>{stock}</h3>
+        <img src={`${image}`} alt={`${image}`}></img>
     </div>
 
     );
